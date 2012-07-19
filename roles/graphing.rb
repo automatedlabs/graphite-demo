dbf91697340a406181b1387fdba09dab552558d8
Jeffrey Hulten <jeffh@automatedlabs.com>
2012-07-19 09:55:51 -0700

    added statsd from working session w/ doug ireton

--- roles/graphing.rb
+++ roles/graphing.rb
@@ -1,6 +1,6 @@
 name "graphing"
 description "The base role for systems that serve graphs"
-run_list "recipe[graphite]"#, "recipe[statsd]"
+run_list "recipe[graphite]", "recipe[statsd]"
 #env_run_lists "prod" => ["recipe[apache2]"], "staging" => ["recipe[apache2::staging]"]
 default_attributes "graphite" => { "password" => "passwordz", "python_version" => "2.7" }
 #override_attributes "apache2" => { "max_children" => "50" }
